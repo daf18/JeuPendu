99b18739e85f135343ba5b7d2d32dd31d0c41ef9
YananGuoguo <yananguoguo@gmail.com>
2022-05-22 14:46:00 -0400

    Initial Commit

---
+++ Pendu_Labo2UITests/Pendu_Labo2UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Pendu_Labo2UITestsLaunchTests.swift
+//  Pendu_Labo2UITests
+//
+//  Created by MAC on 2022-05-22.
+//
+
+import XCTest
+
+class Pendu_Labo2UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
